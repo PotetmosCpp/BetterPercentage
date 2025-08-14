aebd1cc1e45bfb3788973b54c76a94671fe1d0b3
aet <alvaro.traelnes@gmail.com>
2025-08-14 23:28:26 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+//TODO: add realtime new best option maybe idk
+
+class $modify(PlayLayer) {
+	struct Fields {
+		float m_runFrom;
+		float m_bestRunEnd;
+		std::unordered_map<double, double> m_bestRunEnds;	// dumb name i think idk
+	};
+
+	void destroyPlayer(PlayerObject* player, GameObject* object) {
+		PlayLayer::destroyPlayer(player, object);
+
+		if (object != m_anticheatSpike) {
+			if (PlayLayer::getCurrentPercent() > m_fields->m_bestRunEnds[m_fields->m_runFrom]) {
+				m_fields->m_bestRunEnds[m_fields->m_runFrom] = PlayLayer::getCurrentPercent();
+			}
+		}
+	}
+
+	void resetLevel() {
+		/*checkBestRun();*/
+
+		PlayLayer::resetLevel();
+
+		m_fields->m_runFrom = PlayLayer::getCurrentPercent();
+		/*geode::createQuickPopup(
+			"Title",            // title
+			"suppe",   // content
+			"Nah", "Yeah",      // buttons
+			[](auto, bool btn2) {
+			}
+		);*/
+	}
+
+	void updateProgressbar() {
+		PlayLayer::updateProgressbar();
+
+		auto decimals = Mod::get()->getSettingValue<int>("decimals");
+		auto showPercentSymbol = Mod::get()->getSettingValue<bool>("show-percent-symbol");
+
+		auto showRunFrom = Mod::get()->getSettingValue<bool>("show-run-from");
+		auto hideRunFromFromZero = Mod::get()->getSettingValue<bool>("hide-run-from-from-zero");
+
+		auto showBestRun = Mod::get()->getSettingValue<bool>("show-best-run");
+		auto showLevelBestFromZero = Mod::get()->getSettingValue<bool>("show-level-best-from-zero");
+
+		auto formatNumber = [decimals](double value) {
+			std::ostringstream ostringstream;
+			ostringstream << std::fixed << std::setprecision(decimals) << value;
+			return ostringstream.str();
+		};
+
+		auto runFrom = (showRunFrom)
+			? ((m_fields->m_runFrom == 0 && hideRunFromFromZero)
+				? ""
+				: formatNumber(m_fields->m_runFrom) + "-"
+			)
+			: "";
+
+		auto bestRun = (showBestRun)
+			? ((m_fields->m_runFrom == 0 && showLevelBestFromZero)
+				? "/" + std::to_string(m_level->m_normalPercent)
+				: ((m_fields->m_bestRunEnds.count(m_fields->m_runFrom))
+			 		? "/" + formatNumber(m_fields->m_bestRunEnds[m_fields->m_runFrom])	// wtf is this
+					: ""
+				)
+	  		)
+			: "";
+
+		auto percent = (showPercentSymbol)
+			? "%"
+			: "";
+
+		m_percentageLabel->setString((
+			runFrom +
+			formatNumber(PlayLayer::getCurrentPercent()) +
+			bestRun +
+			percent
+		).c_str());
+	}
+};
